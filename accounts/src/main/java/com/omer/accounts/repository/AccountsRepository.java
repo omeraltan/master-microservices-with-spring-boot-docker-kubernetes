eab895ab40fdc9c3a7718448513e9bf05b969389
omeraltan <omeraltan66@gmail.com>
2024-10-30 14:33:18 +0300

    Commit All Changes

--- accounts/src/main/java/com/omer/accounts/repository/AccountsRepository.java
+++ accounts/src/main/java/com/omer/accounts/repository/AccountsRepository.java
@@ -1,9 +1,32 @@
 package com.omer.accounts.repository;
 
 import com.omer.accounts.entity.Accounts;
+
+import jakarta.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+    
+    Optional<Accounts> findByCustomerId(Long customerId);
+
+    @Transactional
+    @Modifying
+    void deleteByCustomerId(Long customerId);
 }
+
+/**
+ * 
+ * Bu Anotasyonların Repository Sınıfında Kullanılma Nedeni
+ * 
+    Repository sınıfında @Transactional ve @Modifying anotasyonlarının kullanılması şu durumlarda uygun olabilir:
+    1. Doğrudan Veri İşlemi Gerektiren Metotlar: Repository'de tanımlanan deleteByCustomerId gibi doğrudan veri silen veya güncelleyen işlemler, işlem (transaction) özelliklerine ihtiyaç duyar. Bu nedenle @Transactional ile işlem yönetimi sağlanır.
+    2. Basit Veri İşlemleri İçin: Sadece bir veritabanı işlemi varsa, service katmanına çıkarmak yerine bu işlemi doğrudan repository katmanında yapmak sade bir yapı sağlar. Bu durumda, @Transactional ve @Modifying anotasyonlarını repository’de kullanmak, daha performanslı ve sade bir yapı sağlar.
+    3. Spring Data JPA'nın Sağladığı Kolaylıklar: Spring Data JPA, repository içinde işlemleri kolayca tanımlamamıza olanak tanır. Eğer işlem service sınıfında gereksiz derecede basit bir görev üstleniyorsa, bu anotasyonları repository’de kullanmak kabul edilebilir.
+ * 
+ */
--- accounts/src/main/java/com/omer/accounts/service/IAccountsService.java
+++ accounts/src/main/java/com/omer/accounts/service/IAccountsService.java
@@ -9,4 +9,25 @@ public interface IAccountsService {
      * @param customerDto - CustomerDto Object
      */
     void createAccount(CustomerDto customerDto);
+
+    /**
+     * 
+     * @param mobileNumber - Input Mobile Number
+     * @return @Account Details on a given mobileNumber
+     */
+    CustomerDto fetchAccount(String mobileNumber);
+
+    /**
+     * 
+     * @param customerDto - CustomerDto Object
+     * @return boolean indicating if the update of Account details is successful or not
+     */
+    boolean updateAccount(CustomerDto customerDto);
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return boolean indicating if the delete of Account details is successful or not
+     */
+    boolean deleteAccount(String mobileNumber);
 }
