612fc5e0675c5aa03e1f97bc17f5e7b31966d79f
omeraltan <omeraltan66@gmail.com>
2024-10-30 17:48:48 +0300

    Commit All Changes

--- accounts/src/main/java/com/omer/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/omer/accounts/AccountsApplication.java
@@ -2,8 +2,36 @@ package com.omer.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Accounts microservice REST API Documentation",
+		description = "Bank Accounts microservice REST API Documentation",
+		version = "v1",
+		contact = @Contact(
+			name = "Ömer ALTAN",
+			email = "omeraltan66@gmail.com",
+			url= "https://github.com/omeraltan"
+		),
+		license = @License(
+			name = "Apache 2.0",
+			url = "https://github.com/omeraltan"
+		)
+	),
+	externalDocs = @ExternalDocumentation(
+			description = "Bank Accounts microservice REST API Documentation",
+			url = "https://www.examplebank.com/swagger-ui.html"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
