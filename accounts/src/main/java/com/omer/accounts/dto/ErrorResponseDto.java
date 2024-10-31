c2186b6e1305a0ce7e41b22df7d0649c5ac947c7
omeraltan <omeraltan66@gmail.com>
2024-10-31 10:09:27 +0300

    Api Documentation for endpoints

--- accounts/src/main/java/com/omer/accounts/dto/ErrorResponseDto.java
+++ accounts/src/main/java/com/omer/accounts/dto/ErrorResponseDto.java
@@ -4,12 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDateTime;
 
+@Schema(
+    name = "ErrorResponse",
+    description = "Schema to hold error response information"
+)
 @Data @AllArgsConstructor
 public class ErrorResponseDto {
+    @Schema(description = "Api path invoked by client")
     private String apiPath;
+    @Schema(description = "Error code representing the error happened")
     private HttpStatus errorCode;
+    @Schema(description = "Error message representing the error happened")
     private String errorMessage;
+    @Schema(description = "Time representing when the error happened")
     private LocalDateTime errorTime;
 }
