9fcf6b74d3e976328769c008e61982475e8da0f6
omeraltan <omeraltan66@gmail.com>
2024-11-09 22:21:31 +0300

    Config.server .yml files structure changed.

--- accounts/src/main/java/com/omer/accounts/dto/AccountsContactInfoDto.java
+++ accounts/src/main/java/com/omer/accounts/dto/AccountsContactInfoDto.java
@@ -3,9 +3,17 @@ package com.omer.accounts.dto;
 import java.util.List;
 import java.util.Map;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupports) {
-    
+@Getter
+@Setter
+public class AccountsContactInfoDto{
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+
 }
