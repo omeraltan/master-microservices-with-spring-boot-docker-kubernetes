7ba022c2b4469268dd8b9a6e5d94834a1bdc2e98
omeraltan <omeraltan66@gmail.com>
2024-11-19 22:14:17 +0300

    Eureka Server added. Also, we used feignClient

--- accounts/src/main/java/com/omer/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/omer/accounts/AccountsApplication.java
@@ -3,6 +3,8 @@ package com.omer.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import com.omer.accounts.dto.AccountsContactInfoDto;
@@ -14,6 +16,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 
 @SpringBootApplication
+@EnableFeignClients
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
 @OpenAPIDefinition(
---
+++ accounts/src/main/java/com/omer/accounts/service/ICustomerService.java
@@ -0,0 +1,14 @@
+package com.omer.accounts.service;
+
+import com.omer.accounts.dto.CustomerDetailsDto;
+
+public interface ICustomerService {
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber);
+
+}
