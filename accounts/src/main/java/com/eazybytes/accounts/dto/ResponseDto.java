29a2bb9deaab310b4d47bf577c4c89d01f36e17e
omeraltan <omeraltan66@gmail.com>
2026-01-04 22:34:22 +0300

    Commit All loans features

--- accounts/src/main/java/com/eazybytes/accounts/dto/ResponseDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/ResponseDto.java
@@ -13,11 +13,11 @@ import lombok.NoArgsConstructor;
 public class ResponseDto {
 
     @Schema(
-        description = "Status code in the response", example = "200"
+        description = "Status code in the response"
     )
     private String statusCode;
     @Schema(
-        description = "Status message in the response", example = "Request processed successfully"
+        description = "Status message in the response"
     )
     private String statusMsg;
 
